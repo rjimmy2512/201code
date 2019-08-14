631d334dc9b09940147241b9372f8f2f34e0a07c
R.James <51843460+rjimmy2512@users.noreply.github.com>
2019-08-13 23:47:42 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,31 @@ if(userType.toLowerCase() === 'no'){
 } else {
   alert('Nooo! You are not meant to be Here!!');
 }
+
+var userGender = prompt('Are You a Male?');
+if(userGender.toLowerCase() === 'yes'){
+  alert('I am a Male, Too.');
+} else {
+  alert('That\'s cool. I am a Male!!');
+}
+
+var userCountry = prompt('Which Country do you live in?');
+if(userCountry.toLowerCase() === 'usa'){
+  alert('Waaw! I live in USA too!!!');
+} else {
+  alert('Waaw. Your country is great. I will visit one day!');
+}
+
+var userLikesCoding = prompt('Do you like Coding?');
+if(userLikesCoding.toLowerCase() === 'yes'){
+  alert('Waaw! It is My Hoobby!!!');
+} else {
+  alert('Try it! It is very interesting!!');
+}
+
+var userWannaGoAhead = prompt('This site is about Coding. Do you want to go ahead and learn more?');
+if(userWannaGoAhead.toLowerCase() === 'yes'){
+  alert('Alright. Go ahead and Click Ok.');
+} else {
+  alert('It is okey. Maybe we will meet again when you change your mind.');
+}
