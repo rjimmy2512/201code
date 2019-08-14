955ae5454c119ed8922321fe473c89671ba52763
R.James <rjimmy2512@gmail.com>
2019-08-13 23:13:28 -0700

    app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var userType = prompt('Hi. Are You not a Robot?');
-// userType = userType.toLowerCase();
+var userName = prompt('Hi. May I know Your Name, Please?');
+log.console('The user name is,' + userName + );
+alert('Hello,' +userName);
 
+var userType = prompt('Hi. Are You not a Robot?');
 if(userType.toLowerCase() === 'no'){
   alert('Welcome to My Website.');
 } else {
