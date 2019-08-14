c58ce4aeb4450e2f50da33a36d3a65cea1bf440e
R.James <51843460+rjimmy2512@users.noreply.github.com>
2019-08-13 23:29:29 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 'use strict';
 
-var userName = prompt('Hi. May I know Your Name, Please?')
-if(userName.toLowerCase() === ''){
-alert('Hello,' +userName);
-}
-
 var userType = prompt('Hi. Are You not a Robot?');
 if(userType.toLowerCase() === 'no'){
   alert('Welcome to My Website.');
