709a1613d4d0185164168e38f93f6662b8dec4a8
R.James <rjimmy2512@gmail.com>
2019-08-13 21:04:37 -0700

    update files

---
+++ js/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+
+var userHuman = prompt('Hi. Are You not a Robot?');
+// userHuman = userHuman.toLowerCase();
+
+if(userHuman === 'no'){
+  alert('Welcome to My Website.');
+} else {
+  alert('Noon! You were not meant to be Here!!');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
+
+var userName = prompt('So, What is your name?');
+    alert('user name is,' + userName);
+    log.console('Thank you, '+ userName +);
+
+ 
+var userGender = prompt('Are You a Male,' + username +'?');
+if(userGender.toLowerCase() === 'YES'){
+  alert('I am a Male too, ' + userName +'. Please click and go ahead.');
+} else {
+  alert('Nice to have you here, ' + userName + '. Click and Go ahead.');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
+
+var userCountry = prompt('Do you live in USA?');
+if(userCountry.toUpperCase() === 'YES'){
+  alert('I live in USA, Too');
+} else {
+  alert('Which country do you live in?');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
+
+var userChoice = prompt('Do you like Coding?');
+if(userChoice.toUpperCase() === 'YES'){
+  alert('I like Coding, Too');
+} else {
+  alert('Not bad. I can teach you some tricks about it!');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
+
+var userChosesToGoAhead = prompt('This Website talks about Coding. Do you like to go ahead?');
+
+if(userChosesToGoAhead.toUpperCase() === 'YES'){
+  alert('Welcome to My Website, ' + userName +'.');
+} else {
+  alert('It is okey. See you if you change your mind!!!');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
