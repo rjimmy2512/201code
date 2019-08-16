8422d2ed83efd8b32c275bdf2465106ae98cdd37
R.James <rjimmy2512@gmail.com>
2019-08-15 19:16:20 -0700

    updated app.js to include console.log

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 'use strict';
 
-var userType = prompt('Hi. Are You not a Robot?');
+var userName = prompt('Hello. What is your name?');
+console.log('The user name is, ' + userName);
+alert('Hello, ' + userName + '.');
+
+
+var userType = prompt('Hi. Are You not a Robot, ' + userName + '?');
+console.log('The user type is, ' + userType);
 if(userType.toLowerCase() === 'no'){
-  alert('Welcome to My Website.');
+  alert('Welcome to My Website, ' + userName + '.');
 } else {
-  alert('Noooo! You were not meant to be Here!!');
+  alert('Noooo! You were not meant to be Here, ' + userName +'!!');
 }
 
-var userGender = prompt('Are You a Male?');
+var userGender = prompt('Are You a Male , ' + userName +'?');
+console.log('The user gender is, ' + userGender);
 if(userGender.toLowerCase() === 'yes'){
   alert('I am a Male, Too.');
 } else {
   alert('That\'s cool. I am a Male!!');
 }
 
-var userCountry = prompt('Which Country do you live in?');
+var userCountry = prompt('Which Country do you live in, ' + userName +'?');
+console.log('The user country is, ' + userCountry);
 if(userCountry.toLowerCase() === 'usa'){
   alert('Waaw! I live in USA too!!!');
 } else {
-  alert('Waaw. Your country is great. I will visit one day!');
+  alert('Waaw. Your country is great. I will visit one day. God willing!');
 }
 
-var userLikesCoding = prompt('Do you like Coding?');
+var userLikesCoding = prompt('Do you like Coding, ' + userName +'?');
+console.log('The user choice is, ' + userLikesCoding);
 if(userLikesCoding.toLowerCase() === 'yes'){
   alert('Waaw! It is My Hoobby!!!');
 } else {
@@ -29,8 +38,10 @@ if(userLikesCoding.toLowerCase() === 'yes'){
 }
 
 var userWannaGoAhead = prompt('This site is about Coding. Do you want to go ahead and learn more?');
+console.log('The user will to go ahead is , ' + userWannaGoAhead);
 if(userWannaGoAhead.toLowerCase() === 'yes'){
-  alert('Alright. Go ahead and Click Ok.');
+  alert('Alright, ' + userName +'. Let\'s go ahead. Click Ok.');
 } else {
-  alert('It is okey. Maybe we will meet again when you change your mind.');
+  alert('It is okey, ' + userName +'. Take a tour on my website maybe you can change your mind. Click Ok.');
 }
+
